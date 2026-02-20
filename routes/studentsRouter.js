7e201b5d11a7f61a078ca8e79350459a3af37813
Lakshitha <lakshithawalakuluarachchi10@gmail.com>
2026-02-20 12:14:56 +0530

    clean code

--- routes/studentsRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-// Example route: GET /students/
-router.get('/', (req, res) => {
-    res.json({ message: 'Students route is working!' });
-});
-
-export default router;
